482483ff78bfe008d2304808a8334035673fc04f
piotrmigas <piotr.migas@icloud.com>
2019-10-25 12:19:13 +0200

    commit

--- src/components/FormUserDetails.js
+++ src/components/FormUserDetails.js
@@ -5,6 +5,7 @@ import TextField from "@material-ui/core/TextField";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogActions from "@material-ui/core/DialogActions";
+import { TextValidator, ValidatorForm } from "react-material-ui-form-validator";
 
 class FormUserDetails extends Component {
   continue = e => {
@@ -18,30 +19,37 @@ class FormUserDetails extends Component {
       <Dialog open="true" fullWidth="true" maxWidth="sm">
         <DialogTitle>Enter User Details</DialogTitle>
         <DialogContent>
-          <TextField
-            autofocus
-            fullWidth
-            margin="dense"
-            label="First Name"
-            onChange={handleChange("firstName")}
-            defaultValue={values.firstName}
-          />
-          <TextField
-            autofocus
-            fullWidth
-            margin="dense"
-            label="Last Name"
-            onChange={handleChange("lastName")}
-            defaultValue={values.lastName}
-          />
-          <TextField
-            label="Email"
-            onChange={handleChange("email")}
-            defaultValue={values.email}
-            autofocus
-            fullWidth
-            margin="dense"
-          />
+          <ValidatorForm>
+            <TextField
+              autofocus
+              fullWidth
+              margin="dense"
+              label="First Name"
+              onChange={handleChange("firstName")}
+              defaultValue={values.firstName}
+              value={values.firstName}
+            />
+            <TextField
+              autofocus
+              fullWidth
+              margin="dense"
+              label="Last Name"
+              onChange={handleChange("lastName")}
+              defaultValue={values.lastName}
+              value={values.lastName}
+            />
+            <TextValidator
+              label="Email"
+              onChange={handleChange("email")}
+              defaultValue={values.email}
+              autofocus
+              fullWidth
+              margin="dense"
+              value={values.email}
+              validators={["isEmail"]}
+              errorMessages={["email is not valid"]}
+            />
+          </ValidatorForm>
         </DialogContent>
         <DialogActions>
           <Button color="primary" variant="contained" onClick={this.continue} style={styles.button}>
